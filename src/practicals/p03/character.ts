1e289f9e1135b04294c0dfb9b9cdbe117144c8c5
panabordee <68010697@kmitl.ac.th>
2026-02-20 18:12:39 +0700

    practice3

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,21 @@
-export class Character {}
+export class Character {
+  protected name: string;
+  protected health: number;
+
+  constructor(name: string, health: number = 100) {
+    this.name = name;
+    this.health = health;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getHealth(): number {
+    return this.health;
+  }
+
+  public receiveDamage(damage: number): void {
+    this.health -= damage;
+  }
+}
--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,20 @@
 import { Character } from "./character";
 
 export class Warrior extends Character {
-  // TODO: implement class properties, constructor with super(...), and methods
+  private weapon: string;
+
+  constructor(name: string, weapon: string, health: number) {
+    super(name, health);
+    this.weapon = weapon;
+  }
+
+  public getWeapon(): string {
+    return this.weapon;
+  }
+
+  // override
+  public receiveDamage(damage: number): void {
+    const reducedDamage = damage * 0.9;
+    this.health -= reducedDamage;
+  }
 }
